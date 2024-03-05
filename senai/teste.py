0d81cee900d46f2b667349c8a7fbba74f72fe139
Rosso <rafael.rosso@thomsonreuters.com>
2024-03-04 21:47:51 -0300

    a

--- senai/teste.py
+++ senai/teste.py
@@ -1,8 +1,8 @@
-import git
+
 import os
 
 msg = input("Digite o titulo de seu commit: ")
-repo = git.Repo(os.getcwd())
+repo = os.getcwd()
 repo.git.pull()
 repo.git.add(".")
 repo.git.commit('-m', msg, author='rafael.rosso.1984@gmail.com')
