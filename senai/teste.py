b26a86dcf94afaa65dab3654d144286d3f11cc29
Rosso <rafael.rosso@thomsonreuters.com>
2024-03-11 18:39:44 -0300

    comitar site

--- senai/teste.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-msg = input("Digite o titulo de seu commit: ")
-repo = os.getcwd()
-repo.git.pull()
-repo.git.add(".")
-repo.git.commit('-m', msg, author='rafael.rosso.1984@gmail.com')
-repo.git.push()
